d3384b250ded3c13ce653a0bac50ccb964a2d5ea
flma0001 <markus.fleschutz@hs-karlsruhe.de>
2021-11-23 09:13:27 +0100

    Bump version: 0.1.2 → 0.1.3

--- draf/__init__.py
+++ draf/__init__.py
@@ -6,7 +6,7 @@ __title__ = "DRAF"
 __summary__ = "Demand Response Analysis Framework"
 __uri__ = "https://github.com/DrafProject/draf"
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 __author__ = "Markus Fleschutz"
 __email__ = "mfleschutz@gmail.com"
