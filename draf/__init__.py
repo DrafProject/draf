5420f6b9cf00c6c730e91a30bde9ca1283003d8c
mfleschutz <mfleschutz@gmail.com>
2023-04-19 07:56:13 +0200

    Bump version: 0.3.0 → 0.3.1

--- draf/__init__.py
+++ draf/__init__.py
@@ -6,7 +6,7 @@ __title__ = "DRAF"
 __summary__ = "Demand Response Analysis Framework"
 __uri__ = "https://github.com/DrafProject/draf"
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 __author__ = "Markus Fleschutz"
 __email__ = "mfleschutz@gmail.com"
